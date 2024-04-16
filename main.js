fec15913fe3e57adb1ffae9a142c9ccaa0b02730
marshadali <m_arshadali@hotmail.com>
2024-04-16 20:08:15 +0500

    First Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+function Avg(num1, num2, num3) {
+    let totalNumber = num1 + num2 + num3;
+    let totalAverage = totalNumber / 3;
+    console.log(totalAverage);
+}
+Avg(4, 3, 5);
---
+++ main.ts
@@ -0,0 +1,7 @@
+function Avg(num1: number, num2 : number, num3 : number)
+{
+    let totalNumber = num1 + num2 + num3;
+    let totalAverage = totalNumber/3;
+    console.log(totalAverage);
+}
+Avg(4,3,5);
